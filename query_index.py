0e289c00f97e875acbf42f6e0ad8cca37286b1c3
客野一樹 <kyakuno@axell.co.jp>
2023-03-17 10:36:50 +0900

    Update query example

--- query_index.py
+++ query_index.py
@@ -4,7 +4,7 @@ os.environ["OPENAI_API_KEY"] = 'YOUR_OPENAI_API_KEY'
 from llama_index import GPTSimpleVectorIndex, LLMPredictor
 from langchain import OpenAI
 
-query_text ="ailia SDKでVulkanを使用するにはどうすれば良いですか。日本語で回答してください。"
+query_text ="ailia SDKが対応しているOSを教えてください。"
 
 llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, max_tokens=350))
 index = GPTSimpleVectorIndex.load_from_disk('index.json')
